2e8019508442111ebce736932fb884240ebe3a31
GoergeKogan <goerge@roi.ad>
2022-06-12 16:26:40 -0400

    crossroads

--- common/aggregations/campaign_conversion_report.js
+++ common/aggregations/campaign_conversion_report.js
@@ -30,6 +30,14 @@ switch(network){
                 GROUP BY sd.domain  
               `);
   
+  case 'crossroads':
+    return db.raw(`  
+                SELECT campaign, 
+                AVG(revenue / CASE clicks::decimal WHEN 0 THEN null ELSE clicks::decimal END)::numeric(10,2) as ave_rpc
+                FROM system1 as s1
+                  INNER JOIN campaigns c ON s1.campaign_id = c.id AND c.traffic_source = 'facebook'
+                GROUP BY s1.campaign  
+              `);
 }
 
   }
