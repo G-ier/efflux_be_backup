62042ee1cd1f57ddef4b749877bdc26ad12d3c98
Goerge <goerge@roi.ad>
2023-02-26 13:52:44 +0100

    update postback sheet

--- common/aggregations/aggregate_ppostback_conversion_by_traffic_report.js
+++ common/aggregations/aggregate_ppostback_conversion_by_traffic_report.js
@@ -7,9 +7,13 @@ const aggregatePpostbackConversionByTrafficReport = (startDate, endDate, network
   SELECT pb.campaign_id as campaign_id,
   MAX(pb.campaign_name) as campaign_name,
   MAX(pb.traffic_source) as traffic_source,
-  SUM(pb.pb_value) as revenue
+  SUM(pb.pb_value) as revenue,
+  COUNT(pb.event_type) as conversions,
   FROM postback_events pb
-  WHERE pb.date > '${startDate}' AND pb.date <= '${endDate}' AND pb.traffic_source = '${traffic_source}' AND network = '${network}'
+  WHERE pb.date > '${startDate}'
+    AND pb.date <= '${endDate}'
+    AND pb.traffic_source = '${traffic_source}'
+    AND network = '${network}'
   GROUP BY pb.campaign_id
 `)
 
