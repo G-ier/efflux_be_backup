c0452e7c4b5c95c468117264e5ea3860618bc7f2
Goerge <goerge@roi.ad>
2023-03-02 09:29:55 -0500

    add sedo sheet tabs for last days

--- constants/sedo.js
+++ constants/sedo.js
@@ -10,6 +10,26 @@ const SEDO_SHEET = [
     fromDay: 2,
     toDay: 1,
   },
+  {
+    network: '',
+    timezone: SEDO_TIMEZONE,
+    spreadsheetId:"1MR5t_Ei7y_C2Jr-sQC0KqZ7SK1vG9R8YqJLbEMW4uIA",
+    sheetName: "Campaign Last 3days",
+    sheetNameByAdset: "Adset Last 3days",
+    traffic: '',
+    fromDay: 4,
+    toDay: 1,
+  },
+  {
+    network: '',
+    timezone: SEDO_TIMEZONE,
+    spreadsheetId:"1MR5t_Ei7y_C2Jr-sQC0KqZ7SK1vG9R8YqJLbEMW4uIA",
+    sheetName: "Campaign Last 7days",
+    sheetNameByAdset: "Adset Last 7days",
+    traffic: '',
+    fromDay: 8,
+    toDay: 1,
+  },
 ]
 const SEDO_SHEET_VALUES = [
   'domain',
