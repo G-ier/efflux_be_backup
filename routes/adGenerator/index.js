a67940c06f33d1d477dc6b66f33a6b24f2956d6b
000507393 <deni.haxhiu13@gmail.com>
2023-06-21 11:14:56 +0200

    Craeted Ad Spy Tool Spreadsheet Generator func

--- routes/adGenerator/index.js
+++ routes/adGenerator/index.js
@@ -25,29 +25,39 @@ function calculateValuesForGeneratedSpreadsheet(data, columns) {
 route.post('/generate-google-spreadsheet',
   wrap(async (req, res) => {
 
-    // const spreadsheetData = req.body;
-    // const columns = ['ad_archive_id', 'ad_starting_date', 'ad_headline', 'ad_primary_text',
-    // 'ad_description', 'ad_publisher_name', 'cdn_url', 'keywords', 'network']
+    const spreadsheetData = req.body;
 
-    // // // Create the spreadsheet with access to the given emails
-    // const emailsWithAccess = ['deni@roi.ad', 'm@roi.ad', 'p@roi.ad', 'losid@roi.ad', 'timothy@roi.ad']
-    // let spreadsheetId = '1YnbgGxKuWoCOLfwA6cpFTT-hlM88G-jNPNae8J4d80A'
+    // Processing keywords
+    for (let i = 0; i < spreadsheetData.length; i++) {
+      const ad = spreadsheetData[i]
+      ad.keywords = ad.keywords.reduce((acc, keyword) => {
+        return acc + (acc !== '' ? ", " : "" ) + keyword
+      }, "")
+    }
+
+    console.log("Request Data Sample", spreadsheetData[0])
+
+    const columns = ['ad_archive_id', 'ad_starting_date', 'ad_headline', 'ad_primary_text',
+    'ad_description', 'ad_publisher_name', 'cdn_url', 'keywords', 'network']
+
+    // Create the spreadsheet with access to the given emails
+    const emailsWithAccess = ['deni@roi.ad', 'm@roi.ad', 'p@roi.ad', 'losid@roi.ad', 'timothy@roi.ad']
+    let spreadsheetId = '1YnbgGxKuWoCOLfwA6cpFTT-hlM88G-jNPNae8J4d80A'
     // // spreadsheetId = await spreadsheets.createSpreadsheet('Scrapping Request', emailsWithAccess)
 
-    // // Check if the spreadsheet was created
-    // if (!spreadsheetId) return res.status(400).json({"message": "Error creating spreadsheet"});
+    // Check if the spreadsheet was created
+    if (!spreadsheetId) return res.status(400).json({"message": "Error creating spreadsheet"});
 
     // // Add the data to the spreadsheet
-    // const spreadSheetPostProcessedData = calculateValuesForGeneratedSpreadsheet(spreadsheetData, columns);
-    // console.log('spreadSheetPostProcessedData', spreadSheetPostProcessedData)
-    // await spreadsheets.updateSpreadsheet(
-    //   spreadSheetPostProcessedData,
-    //   {spreadsheetId, sheetName: 'Sheet1'},
-    //   predifeniedRange = "",
-    //   include_columns = true,
-    //   add_last_update = false
-    //   )
-    return res.status(200).json('temporarily-empty');
+    const spreadSheetPostProcessedData = calculateValuesForGeneratedSpreadsheet(spreadsheetData, columns);
+    await spreadsheets.updateSpreadsheet(
+      spreadSheetPostProcessedData,
+      {spreadsheetId, sheetName: 'Sheet1'},
+      predifeniedRange = "",
+      include_columns = true,
+      add_last_update = false
+      )
+    return res.status(200).json(spreadsheetId);
   }),
 );
 
