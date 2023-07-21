150a351aebc454b1d73219d9bb3af8e5495a1db2
000507393 <deni.haxh1303@gmail.com>
2023-07-21 18:52:45 +0200

    Removed active accounts sheet on fb revealbot

--- cron/aggreagetedSheet-cron.js
+++ cron/aggreagetedSheet-cron.js
@@ -16,72 +16,12 @@ const disableCron = process.env.DISABLE_CRON === 'true'
 const everyFifteenMinutes = '14-59/15 * * * *';
 const everyXminutes = '58 * * * *';
 
-function mapActiveAccountValue(data) {
 
-  return data.map(row => {
-    let processedRow = row.length === 10 ? row.slice(0, 10) : row
-    let dateTime = new Date(processedRow[9])
-    let date = new Date()
-
-    return {
-      '' : processedRow[0],
-      "Ad Account name": processedRow[1],
-      "Ad Account ID": processedRow[2],
-      "Time Zone": processedRow[3],
-      "Currency": processedRow[4],
-      "Maximum Spent": processedRow[5],
-      "Account Limit spent": processedRow[6],
-      "Today Spent": processedRow[7],
-      "Remaining Balance": processedRow[8],
-      "Referred Sheet Update Time": `${date.getDate()}/${dateTime.getHours()}:${dateTime.getMinutes() < 10 ? '0' + dateTime.getMinutes() : dateTime.getMinutes()}`,
-      "Last Updated": `${date.getDate()}/${date.getHours()}:${date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()}`
-    }
-  })
-}
-
-const updateActiveAccountsTab = async () => {
-  const spreadsheetId = "1J1S8hO4Ju_4z1-9B1MVnyESDGEST2aoMYfMTo8cUd2M";
-  const sheetName = "Active Accounts";
-
-  const writeSpreadsheetId = "1A6Pfy2GPq0z12b_CtDdaMb5zKWecJa2jkzLVA3BwBuQ"
-  const writeSheetName = "Active Accounts"
-
-  const range = sheetName;
-
-  // get sheet values
-  const { values: sheetValues } = await getSheetValues(spreadsheetId, {
-    range
-  });
-
-  // Removing columns from the sheet.
-  const data =  sheetValues.slice(1, sheetValues.length)
-
-  // Converting sheet list rows to objects.
-  const columns = ["", "Ad Account name", "Ad Account ID", "Time Zone", "Currency",  "Maximum Spent", "Account Limit spent", "Today Spent", "Remaining Balance", "Referred Sheet Update Time", "Last Updated"];
-  const rows = mapActiveAccountValue(data)
-
-  const processedData = {
-    columns,
-    rows
-  }
-
-  // Updating spreadsheet
-  await updateSpreadsheet(
-    processedData,
-    {spreadsheetId: writeSpreadsheetId, sheetName: writeSheetName},
-    predifeniedRange = "",
-    include_columns = true,
-    add_last_update = false
-  )
-
-}
 
 const updateAggregatedSheet = async () => {
 
   try {
 
-      // Update the active accounts sheet
-      await updateActiveAccountsTab()
 
       for (i=0; i < sheetsArr.length; i ++ ) {
 
@@ -114,6 +54,7 @@ const updateAggregatedSheet = async () => {
   }
   catch (err) {
       await sendSlackNotification(`Fb Revealbot Sheets.\nError on update: \n${err.toString()}`)
+      console.log(err)
   }
 }
 
