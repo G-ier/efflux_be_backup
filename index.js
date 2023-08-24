8c1c8f9a4d32f2c96daab08d0724d0073b5d6cbf
000507393 <deni.haxh1303@gmail.com>
2023-08-24 12:16:38 +0200

    Fixing staging attempt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const { server, app_htttps, app_htttp } = require("./api/server");
+const { server } = require("./api/server");
 const Sentry = require("@sentry/node");
 
 Sentry.init({
@@ -11,24 +11,21 @@ Sentry.init({
   // We recommend adjusting this value in production
   tracesSampleRate: 1.0,
 });
+
 // 404
 server.use(function (req, res, next) {
   return res
     .status(404)
     .send({ message: "[Route] --> " + req.url + " <-- Not found." });
 });
+
 // 500 - Any server error
 server.use(function (err, req, res, next) {
   return res.status(500).json({ error: err });
 });
 
 const port = process.env.PORT || 5000;
-const http_port = process.env.HTTP_PORT || 4999;
 
-app_htttp.listen(http_port, () =>
-  console.log(`🔥 -------- HTTP listening on port ${http_port} ---------- 🔥`)
-);
-
-app_htttps.listen(port, () =>
-  console.log(`🔥 -------- HTTPS listening on port ${port} ---------- 🔥`)
+server.listen(port, () =>
+  console.log(`🔥 -------- Server listening on port ${port} ---------- 🔥`)
 );
