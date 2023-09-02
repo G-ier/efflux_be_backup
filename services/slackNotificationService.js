3869fc0c5a53892bf85decf504d206d089b4d66f
000507393 <deni.haxh1303@gmail.com>
2023-09-02 20:03:41 +0200

    Debugging

--- services/slackNotificationService.js
+++ services/slackNotificationService.js
@@ -4,9 +4,20 @@ const axios = require('axios');
 const token = "xoxb-3534771044320-5197636119910-5haDnl6FYADYdnV4T55qPK5D"
 const SLACK_API_URL = "https://hooks.slack.com/services/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"
 
+async function getPublicIP() {
+  try {
+    const response = await axios.get('https://api.ipify.org?format=json');
+    return response.data.ip;
+  } catch (error) {
+    console.error('Error fetching public IP:', error);
+  }
+}
+
+
 const sendSlackNotification = async (message) => {
+  const publicIp = await getPublicIP();
   const payload = {
-    text: message
+    text: 'staging' + publicIp + message
   }
   const data = await axios.post(
     SLACK_API_URL,
