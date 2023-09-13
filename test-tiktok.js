008e79b203be96777ce199cd3ba35b5faa8e5135
000507393 <deni.haxh1303@gmail.com>
2023-09-13 08:33:17 +0200

    Trigger Build

--- test-tiktok.js
+++
@@ -1,9 +0,0 @@
-const _ = require("lodash");
-const CompositeService    = require("./src/modules/tiktok/services/CompositeService");
-
-const main = async () => {
-  const compositeService = new CompositeService();
-  await compositeService.updateTikTokData("2023-08-29")
-}
-
-main()
