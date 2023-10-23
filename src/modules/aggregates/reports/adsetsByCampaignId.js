397058d439a332e7f79b7f5c18677e2eb12085c1
Kevi14 <kevikostandini@gmail.com>
2023-10-23 09:54:06 +0200

    Modified the endpoint to return also data for multiple campagins (#111)

---
+++ src/modules/aggregates/reports/adsetsByCampaignId.js
@@ -0,0 +1,28 @@
+const { buildSelectionColumns, castSum } = require("./utils");
+
+async function adsetsByCampaignId(database, startDate, endDate, campaignIds=[]) {
+
+    const campaignIdCondition = campaignIds.length ? `AND insights.campaign_id IN (${campaignIds.map(id => `'${id}'`).join(', ')})` : "";
+
+  const query = `
+    SELECT
+      insights.campaign_id,
+      insights.adset_id,
+      MAX(campaign_name) as campaign_name,
+      MAX(adsets.status) as status,
+      CAST(COALESCE(MAX(adsets.daily_budget), '0') AS FLOAT) as daily_budget,
+      MAX(insights.adset_name) as adset_name,
+      ${castSum(`cr_uniq_conversions`)} as cr_uniq_conversions,
+      ${buildSelectionColumns("", calculateSpendRevenue=true)}
+    FROM insights
+    JOIN adsets ON insights.adset_id = adsets.provider_id
+    WHERE date > '${startDate}' AND date <= '${endDate}' 
+      ${campaignIdCondition}
+    GROUP BY insights.campaign_id, insights.adset_id;
+  `;
+
+  const { rows } = await database.raw(query);
+  return rows;
+}
+
+module.exports = adsetsByCampaignId;
