3030986cdd373be9ab8026702012f42715016f77
000507393 <deni.haxh1303@gmail.com>
2023-11-01 12:02:54 +0100

    Application Start Env Fix

--- deployment_scripts/application_start.sh
+++ deployment_scripts/application_start.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 cd /var/www/efflux-backend
 
-NODE_PORT=5000
-NODE_ENV=production
 pm2 stop ecosystem.config.js
-pm2 start ecosystem.config.js
+pm2 start ecosystem.config.js --env production
 
 # Save the current process list so that it can be restored on reboot
 pm2 save
