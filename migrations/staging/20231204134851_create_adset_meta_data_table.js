ce963d8d292d4a6bca4af0df7fa934b7b5dffba3
Kevin Kostandini <kevikostandini@gmail.com>
2023-12-05 22:22:22 +0100

    Added migrations for metadata

---
+++ migrations/staging/20231204134851_create_adset_meta_data_table.js
@@ -0,0 +1,27 @@
+// migration for adset_metadata table
+
+exports.up = function(knex) {
+    return knex.schema.createTable('adset_metadata', table => {
+      table.increments('id').primary();
+      table.string('name');
+      table.string('status');
+      table.decimal('daily_budget');
+      table.text('special_ad_categories'); // Storing as JSON string
+      table.string('special_ad_categorie_country');
+      table.string('dsa_beneficiary');
+      table.string('dsa_payor');
+      table.string('optimization_goal');
+      table.string('billing_event');
+      table.boolean('is_dynamic_creative');
+      table.text('promoted_object'); // Storing as JSON string
+      table.text('targeting'); // Storing as JSON string
+      table.text('attribution_spec'); // Storing as JSON string
+      table.string('adset_id');
+      table.timestamps(true, true); // Creates created_at and updated_at columns
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('adset_metadata');
+  };
+  
