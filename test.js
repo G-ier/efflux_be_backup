3a565ef7e5673aec3cbc0c7eac168c7601199436
Kebi <kevikostandini@gmail.com>
2023-12-22 14:54:13 +0100

    Added time as well

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
+const { default: axios } = require("axios");
 const AggregatesService =require("./src/modules/aggregates/services/AggregatesService");
 const updateCrossroadsForAllAccounts= require("./src/modules/crossroads/crons");
 const main =async () =>{
-    // updateCrossroadsForAllAccounts('2023-11-22')
-console.log("DONE")
+    const url = 'https://7yhdw8l2hf.execute-api.us-east-1.amazonaws.com/'; // The API endpoint
+const response = await axios.get(url)
+console.log(response.data)
 }
 main()
