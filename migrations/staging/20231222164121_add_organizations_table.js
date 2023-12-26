f9a518d4736c16cbd6f58b0b8869ddc27a189624
iLyas Bakouch <isbkch@gmail.com>
2023-12-26 18:15:10 -0500

    feat(ROI-332): migrations and seeds

--- migrations/staging/20231222164121_add_organizations_table.js
+++ migrations/staging/20231222164121_add_organizations_table.js
@@ -1,10 +1,12 @@
 exports.up = function (knex) {
   return knex.schema.createTable('organizations', function (table) {
-    table.increments('id');
+    table.increments('id').primary();
     table.string('name').notNullable();
+    table.integer('admin_id').unsigned();
     table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
     table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
     table.boolean('is_active').notNullable().defaultTo(true);
+    table.foreign('admin_id').references('users.id');
   });
 };
 
---
+++ seeds/seed_organizations.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex('organizations').del();
+  await knex('organizations').insert([
+    {
+      name: 'ROI AD',
+      admin_id: 3,
+    },
+
+    {
+      name: 'Digitopia',
+      admin_id: 2,
+    },
+  ]);
+};
