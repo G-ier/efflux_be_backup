a4a096d70ac9cb9852fa45270cebb81022ad7ca1
iLyas Bakouch <isbkch@gmail.com>
2023-12-30 15:47:49 -0500

    Add EMAILS_SERVICE_ENDPOINT and SQS_PUSHER credentials

---
+++ src/shared/lib/SQSPusher.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  accessKeyId: process.env.SQS_PUSHER_ACCESS_KEY_ID,
+  secretAccessKey: process.env.SQS_PUSHER_SECRET_KEY,
+  region: 'us-east-1',
+});
+
+// Now you can create service clients or call AWS services.
+class SqsService {
+  constructor(queueUrl) {
+    this.sqs = new AWS.SQS();
+    this.queueUrl = queueUrl;
+  }
+
+  async push(event) {
+    const params = {
+      MessageBody: JSON.stringify(event),
+      QueueUrl: this.queueUrl,
+    };
+
+    try {
+      const data = await this.sqs.sendMessage(params).promise();
+      console.log(`Message sent to SQS queue, ID: ${data.MessageId}`);
+    } catch (error) {
+      console.error(`Error sending message to SQS queue: ${error}`);
+    }
+  }
+}
+
+module.exports = SqsService;
