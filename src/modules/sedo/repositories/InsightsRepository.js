14b5be80c60fb2477be1e7136c44d86c31b43b06
Deni <deni.haxh1303@gmail.com>
2024-01-07 12:54:31 +0100

    Sedo Callback Processor Fix (#206)
    1. Added Facebook Conversion Routing Capabilities.
    2. Added template interpretation of Sedo API data.

--- src/modules/sedo/repositories/InsightsRepository.js
+++ src/modules/sedo/repositories/InsightsRepository.js
@@ -4,7 +4,8 @@ const _ = require('lodash');
 // Local application imports
 const DatabaseRepository = require('../../../shared/lib/DatabaseRepository');
 const Insights = require('../entities/Insights');
-const { isNotNumeric } = require('../../../shared/helpers/Utils');
+const interpretDataWTemplateV1 = require('../templates/v1');
+const interpretDataWTemplateV2 = require('../templates/v2');
 
 class InsightsRepository {
   // We might need to do some processing on the data, aggregate it, etc.
@@ -66,38 +67,8 @@ class InsightsRepository {
   }
 
   parseSedoAPIData(insight, date) {
-    const domain = insight.domain[0]._;
-
-    const funnel_id = insight.c1[0]?._ ? insight.c1[0]._ : '';
-    let [campaign_id, adset_id, ad_id, traffic_source] = insight.c2[0]?._
-      ? insight.c2[0]._.replace(' ', '').split('|')
-      : ['Unkown', 'Unkown', 'Unkown', 'Unkown'];
-
-    if (isNotNumeric(campaign_id)) campaign_id = 'Unkown';
-    if (isNotNumeric(adset_id)) adset_id = 'Unkown';
-    if (isNotNumeric(ad_id)) ad_id = 'Unkown';
-    if (!['tiktok', 'facebook'].includes(traffic_source)) traffic_source = 'Unkown';
-
-    const hit_id = insight.c3[0]?._ ? insight.c3[0]._ : '';
-
-    const visitors = insight.uniques[0]._ ? parseInt(insight.uniques[0]._) : 0;
-    const conversions = insight.clicks[0]._ ? parseInt(insight.clicks[0]._) : 0;
-    const revenue = insight.earnings[0]._ ? parseFloat(insight.earnings[0]._) : 0;
-
-    return {
-      date,
-      domain,
-      traffic_source,
-      campaign_id,
-      adset_id,
-      ad_id,
-      funnel_id,
-      hit_id,
-      visitors,
-      conversions,
-      revenue: revenue,
-      unique_identifier: `${campaign_id}-${adset_id}-${ad_id}-${date}`,
-    };
+    const templateInterpretation = insight.c3[0]?._ && insight.c3[0]?._.startsWith('temp_v2_') ? interpretDataWTemplateV2 : interpretDataWTemplateV1;
+    return templateInterpretation(insight, date);
   }
 
   toDomainEntity(dbObject) {
