384ed5fdf4bf5ec5ab1198cb1f1071eb4b2f3574
iLyas Bakouch <isbkch@gmail.com>
2024-01-09 18:58:45 -0500

    Update application_start.sh to use environment variable for pm2 start command

--- deployment_scripts/application_start.sh
+++ deployment_scripts/application_start.sh
@@ -2,8 +2,12 @@
 
 cd /var/www/efflux-backend
 
+# get the environment from the environment variable passed from the CodePipeline
+# this will be either staging, production, dus_staging or dus_production
+ENV=$1
+
 pm2 stop ecosystem.config.js
-pm2 start ecosystem.config.js --env production
+pm2 start ecosystem.config.js --env $ENV
 
 # Save the current process list so that it can be restored on reboot
 pm2 save
