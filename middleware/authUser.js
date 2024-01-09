551e25c6150e3e2af8238c906645fcb8c93fb49b
Kevi14 <kevikostandini@gmail.com>
2024-01-09 15:48:33 +0100

    Fixed an issue when creating new user from auth0

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -1,9 +1,22 @@
-const UserService = require("../src/modules/auth/services/UserService");
+const UserService = require('../src/modules/auth/services/UserService');
+const _ = require('lodash');
 
 module.exports = async (req, res, next) => {
   req.user.isAdmin = req.user.permissions.includes('admin');
   req.user.providerId = req.user.sub.split('|')[1];
-  const user = await new UserService().fetchOne(['*'], {providerId: req.user.providerId});
-  req.user = {...req.user, ...user}
+  let user = await new UserService().fetchOne(['*'], { providerId: req.user.providerId });
+  if (user) {
+    user.roles = _.compact(user.roles);
+    // Then, check if user.roles is empty
+    if (_.isEmpty(user.roles)) {
+      user.roles = [];
+    }
+    user.permissions = _.compact(user.permissions);
+    // Then, check if user.roles is empty
+    if (_.isEmpty(user.permissions)) {
+      user.permissions = [];
+    }
+  }
+  req.user = { ...req.user, ...user };
   next();
-}
+};
