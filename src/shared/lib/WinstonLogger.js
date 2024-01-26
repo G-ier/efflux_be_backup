76f732a3e34c0912937649a69621d4a452b17c58
iLyas Bakouch <isbkch@gmail.com>
2024-01-26 18:32:39 -0500

    Remove unused cron rules

--- src/shared/lib/WinstonLogger.js
+++ src/shared/lib/WinstonLogger.js
@@ -220,14 +220,6 @@ const AnalysisLogger = new CustomLogger({
   logStreamName: 'analysis',
 });
 
-// CRON data update Logger
-const dataUpdatesLogger = new CustomLogger({
-  destination: streamDestination,
-  level: 'info',
-  logGroupName: '/aws/ec2/efflux-be-' + loggingEnv,
-  logStreamName: 'updates',
-});
-
 // User Logger
 const UserLogger = new CustomLogger({
   destination: streamDestination,
@@ -304,7 +296,6 @@ module.exports = {
   PostbackTestLogger,
   RequestsLogger,
   AnalysisLogger,
-  dataUpdatesLogger,
   SedoLogger,
   TonicLogger,
   MediaNetLogger,
