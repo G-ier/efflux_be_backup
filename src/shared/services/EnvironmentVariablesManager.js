d41ad69d29da9acf185aca327a80ca9f85c804cc
iLyas Bakouch <isbkch@gmail.com>
2024-01-29 16:55:14 -0500

    Add CRON_ENVIRONMENT variable and validate its value

--- src/shared/services/EnvironmentVariablesManager.js
+++ src/shared/services/EnvironmentVariablesManager.js
@@ -191,6 +191,7 @@ class EnvironmentVariablesManager {
       }
     } else {
       const requiredEnvVariables = [
+        'CRON_ENVIRONMENT',
         'DATABASE_ENVIRONMENT',
         'REDIS_ENVIRONMENT',
         'NEWRELIC_ENVIRONMENT',
@@ -214,6 +215,7 @@ class EnvironmentVariablesManager {
           Please ensure it's/they are set in ${envFilePath}.
 
           Example:
+          CRON_ENVIRONMENT=staging
           DATABASE_ENVIRONMENT=staging
           REDIS_ENVIRONMENT=staging
           NEWRELIC_ENVIRONMENT=staging
@@ -222,6 +224,8 @@ class EnvironmentVariablesManager {
       }
 
       // Throw an error if any of the required env variables is invalid
+      if (!['staging', 'production'].includes(envConfig['CRON_ENVIRONMENT']))
+        throw new Error(`CRON_ENVIRONMENT must be either 'staging' or 'production'`);
       if (!['staging', 'production', 'development'].includes(envConfig['DATABASE_ENVIRONMENT']))
         throw new Error(
           `DATABASE_ENVIRONMENT must be either 'staging', 'production' or 'development'`,
