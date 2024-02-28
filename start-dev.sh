4228ffbf629b3b33bcf8d05934026a913a44e65c
iLyas Bakouch <isbkch@gmail.com>
2024-02-28 16:30:22 -0500

    Update Docker and package.json dependencies

--- start-dev.sh
+++ start-dev.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker run -p 6379:6379 -it redis/redis-stack-server:latest &
+open -a Docker
+wait 5
+docker run -p 6379:6379 -it redis/redis-stack-server:latest | spacer &
 npm install
-npm run server 
+npm run server | spacer
