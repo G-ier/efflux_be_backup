87cffb61d4c16c0c709a5562bc6c0aedaae2555e
Shadid-Haque <shadid@roi.ad>
2024-03-22 00:42:18 -0400

    add sqs subscription to backend

---
+++ sqs/index.js
@@ -0,0 +1,76 @@
+const { SQSClient, ReceiveMessageCommand, DeleteMessageCommand } = require("@aws-sdk/client-sqs");
+const sqsClient = new SQSClient({ region: "us-east-1" });
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
+const s3Client = new S3Client({ region: "us-east-1" });
+const AdLauncherService = require("../src/modules/adLauncher/service/AdLauncherService");
+
+// TODO: Update these values to use AWS parameter store or environment variables
+const queueUrl = "https://sqs.us-east-1.amazonaws.com/524744845066/event-from-media-master";
+const bucketName = "efflux-media-master";
+
+async function processMessage(message) {
+  const adLauncherService = new AdLauncherService();
+  try {
+    console.debug("Raw message:", message);
+    console.debug(`✅ Processing message: ${message.MessageId}`);
+    // parse SQS message
+    const messageBody = JSON.parse(message.Body);
+    console.debug("Message Body:", messageBody);
+    const parsedBody = JSON.parse(messageBody.Message);
+    console.debug("parsedBody Message Body:", parsedBody);
+
+    // process the message
+    const { type, adAccountId, s3Key } = parsedBody;
+
+    const getObjectParams = { Bucket: bucketName, Key: s3Key };
+    const imageBuffer = await getS3Object(getObjectParams);
+
+    adLauncherService.uploadMediaToFacebook(type, adAccountId, imageBuffer);
+
+  } catch (error) {
+    console.log(`Error processing message: ${error}`);
+  }
+}
+
+async function getS3Object(getObjectParams) {
+  const stream = await s3Client.send(new GetObjectCommand(getObjectParams)).then((response) => response.Body);
+
+  return new Promise((resolve, reject) => {
+    const chunks = [];
+    stream.on("data", (chunk) => chunks.push(chunk));
+    stream.once("end", () => resolve(Buffer.concat(chunks)));
+    stream.once("error", reject);
+  });
+}
+
+
+async function pollSQSQueue() {
+  try {
+    const receiveParams = {
+      QueueUrl: queueUrl,
+      MaxNumberOfMessages: 5, // Adjust based on throughput needs
+      WaitTimeSeconds: 20, // Long polling
+    };
+
+    while (true) {
+      const receivedMessages = await sqsClient.send(new ReceiveMessageCommand(receiveParams));
+
+      if (receivedMessages.Messages && receivedMessages.Messages.length > 0) {
+        for (const message of receivedMessages.Messages) {
+          await processMessage(message);
+
+          // Delete the message from the queue
+          const deleteParams = {
+            QueueUrl: queueUrl,
+            ReceiptHandle: message.ReceiptHandle,
+          };
+          await sqsClient.send(new DeleteMessageCommand(deleteParams));
+        }
+      }
+    }
+  } catch (error) {
+    console.error("An error occurred while polling the queue:", error);
+  }
+}
+
+module.exports = { pollSQSQueue };
---
+++ src/modules/adLauncher/service/AdLauncherService.js
@@ -0,0 +1,12 @@
+class AdLauncherService {
+  constructor() {
+  }
+
+  async uploadMediaToFacebook(type, adAccountId, imageBuffer) {
+    // TODO: Implement this method
+    console.log(`Uploading media to Facebook...`);
+    console.log(`imageBuffer: --> ${imageBuffer}`);
+  }
+}
+
+module.exports = AdLauncherService;
