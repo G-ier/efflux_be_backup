08bc3b1200658bf7d0910fc25f4adbc129917422
Shadid-Haque <shadid@roi.ad>
2024-03-22 00:55:04 -0400

    submit add to facebook v2

--- sqs/index.js
+++ sqs/index.js
@@ -4,7 +4,6 @@ const AdLauncherService = require("../src/modules/adLauncher/service/AdLauncherS
 
 // TODO: Update these values to use AWS parameter store or environment variables
 const queueUrl = "https://sqs.us-east-1.amazonaws.com/524744845066/event-from-media-master";
-const bucketName = "efflux-media-master";
 
 async function processMessage(message) {
   const adLauncherService = new AdLauncherService();
@@ -17,25 +16,14 @@ async function processMessage(message) {
     const parsedBody = JSON.parse(messageBody.Message);
     console.debug("parsedBody Message Body:", parsedBody);
 
-    // process the message
+    // Final step submit app to facebook
     const { type, adAccountId, s3Key } = parsedBody;
 
-    adLauncherService.submitFacebookAd(type, adAccountId, imageBuffer);
+    adLauncherService.submitAdToFacebook(parsedBody);
 
   } catch (error) {
     console.log(`Error processing message: ${error}`);
   }
-}
-
-async function getS3Object(getObjectParams) {
-  const stream = await s3Client.send(new GetObjectCommand(getObjectParams)).then((response) => response.Body);
-
-  return new Promise((resolve, reject) => {
-    const chunks = [];
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.once("end", () => resolve(Buffer.concat(chunks)));
-    stream.once("error", reject);
-  });
 }
 
 
