caf2607e192cdaec04159bca4262201a15485291
000507393 <deni.haxh1303@gmail.com>
2024-04-02 21:13:05 +0200

    appspec script update

--- deployment_scripts/application_start.sh
+++ deployment_scripts/application_start.sh
@@ -3,7 +3,7 @@
 cd /var/www/efflux-backend
 
 # get the environment from the environment variable passed from the CodePipeline
-# this will be either staging, production, dus_staging or dus_production
+# this will be either staging, production, cronus_staging or cronus_production
 ENV=$1
 
 # if the environment variable is not set, default to production
@@ -12,7 +12,6 @@ then
     ENV=production
 fi
 
-pm2 stop ecosystem.config.js
 pm2 start ecosystem.config.js --env $ENV
 
 # Save the current process list so that it can be restored on reboot
