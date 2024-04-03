893c1168ae7dd987fd7362c44646fd234c2eea87
iLyas Bakouch <isbkch@gmail.com>
2024-04-03 11:32:35 -0400

    Add Mediamaster to .vscode/settings.json and update SnsTopicArn and DynamodbTableName in sns.js

--- serverless/campaign-launch-trigger/handlers/sns.js
+++ serverless/campaign-launch-trigger/handlers/sns.js
@@ -6,33 +6,48 @@ const { unmarshall } = require('@aws-sdk/util-dynamodb');
 
 const sqsClient = new SQSClient({ region: 'us-east-1' });
 
+const SnsTopicArn =
+  process.env.SNS_TOPIC_ARN ||
+  'arn:aws:sns:us-east-1:524744845066:Mediamaster-Downstream-Notifications';
+
 const SqsQueueUrl =
   process.env.SQS_QUEUE_URL ||
   'https://sqs.us-east-1.amazonaws.com/524744845066/ready-to-launch-campaigns';
 
-  /** fet */
+const DynamodbTableName = process.env.DYNAMODB_TABLE_NAME || 'in-progress-campaigns';
+
+/**
+ * Step 1: Receives message from SNS topic
+ * Step 2: queries dynamoDB table for the campaign details using the key "internal_campaign_id" from the message payload
+ * Step 3: if the status field is "published" then sends a message to SQS queue
+ * @param {Object} event - SNS message
+ * @returns {Promise<string>}
+ * @example
+ * {
+ *  "Records": [
+ *   {
+ *    "EventSource": "aws:sns",
+ *   "EventVersion": "1.0",
+ *  "EventSubscriptionArn": "arn:aws:sns:us-east-1:524744845066:Mediamaster-Downstream-Notifications:4b5d6b9c-4e3b-4c4b-8c5b-3c4b5d6e7b8c",
+ * "Sns": {
+ * "Type": "Notification",
+ * "MessageId": "e1b5c2d4-5b6c-4d5e-8b5c-2d4e5b6c7d8e",
+ * "TopicArn": "arn:aws:sns:us-east-1:524744845066:Mediamaster-Downstream-Notifications",
+ * "Subject": "Campaign Published",
+ * "Message": "{\"internal_campaign_id\":\"1234\"}",
+ * "Timestamp": "2021-10-14T20:45:19.000Z",
+ * "SignatureVersion": "1",
+ * "Signature": "EXAMPLE",
+ * "SigningCertUrl": "EXAMPLE",
+ * "UnsubscribeUrl": "EXAMPLE",
+ * "MessageAttributes": {}
+ *
+ */
 exports.handler = async (event) => {
   console.debug('Event: ', JSON.stringify(event, null, 2));
 
-  // Process each record in the event
-  for (let record of event.Records) {
-    // Check if the record is an INSERT event
-    if (record.eventName === 'INSERT') {
-      // Use `unmarshall` to convert the DynamoDB format to a standard JavaScript object
-      const executionLogRow = unmarshall(record.dynamodb.NewImage);
-
-      console.debug('Execution log row:', executionLogRow);
-
-      const fetchingKeyType = executionLogRow.type === 'spend' ? 'partitionKey' : 'rangeKey';
-
-
-
-          // Send message to SQS
-          await sendMessageToQueue(message);
-        }
-      }
-    }
-  }
+  // Send message to SQS
+  await sendMessageToQueue(message);
 
   return `Successfully processed ${event.Records.length} records.`;
 };
