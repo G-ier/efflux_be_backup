6000a92490eb1eead17df9166c807c93c91a8d71
iLyas Bakouch <isbkch@gmail.com>
2024-04-11 13:15:10 -0400

    Update ecosystem.config.js to use fork mode with 1 instance and reduce memory restart size

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,8 +5,8 @@ module.exports = {
       script: 'node -r newrelic index.js',
       ignore_watch: ['./node_modules', './logs', './storage', 'newrelic_agent.log'],
       watch: false,
-      instances: 'max',
-      exec_mode: 'cluster',
+      instances: '1',
+      exec_mode: 'fork',
       max_memory_restart: '512M',
       env: {
         NODE_ENV: 'development',
