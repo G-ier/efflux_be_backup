acac5ff8f39fd6883c0e26077e214c032ad30d23
Deni <deni.haxhiu13@gmail.com>
2024-08-26 15:36:15 +0200

    altered query

--- src/modules/aggregates/reports/networkCampaignsView.js
+++ src/modules/aggregates/reports/networkCampaignsView.js
@@ -8,21 +8,15 @@ async function networkCampaignData(database, startDate, endDate, mediaBuyer, net
       MAX(r.network) AS network,
       r.network_campaign_id AS network_campaign_id,
       MAX(r.network_campaign_name) AS network_campaign_name,
-      SUM(r.landings) AS landings,
-      SUM(r.keyword_clicks) AS keyword_clicks,
-      SUM(r.conversions) AS conversions,
-      SUM(r.revenue) AS revenue,
-      CASE
-          WHEN COUNT(DISTINCT final) = 1 AND MAX(final) IS NOT NULL THEN MAX(final)
-          ELSE 'not_final'
-      END AS final,
-      MAX(r.account) AS account
+      CAST(SUM(r.landings) AS FLOAT) AS landings,
+      CAST(SUM(r.keyword_clicks) AS FLOAT) AS keyword_clicks,
+      CAST(SUM(r.conversions) AS FLOAT) AS nw_conversions,
+      CAST(SUM(r.revenue) AS FLOAT) AS revenue
     FROM
       revenue r
     WHERE
       DATE(r.occurred_at AT TIME ZONE 'UTC' AT TIME ZONE 'America/Los_Angeles') > '${startDate}'
       AND DATE(r.occurred_at AT TIME ZONE 'UTC' AT TIME ZONE 'America/Los_Angeles') <= '${endDate}'
-      ${network ? `AND r.network = '${network}'` : ''}
     GROUP BY
       r.network_campaign_id
   )
@@ -32,17 +26,16 @@ async function networkCampaignData(database, startDate, endDate, mediaBuyer, net
     ra.network_campaign_name as name,
     ra.landings,
     ra.keyword_clicks,
-    ra.conversions,
-    ra.revenue,
-    ra.final,
-    ra.account
-
+    ra.nw_conversions,
+    ra.revenue
   FROM
     revenue_aggregated ra
-  JOIN
-    network_campaigns_user_relations ncur ON ra.network_campaign_id = ncur.network_campaign_id
+  ${
+    mediaBuyer !== "admin" && mediaBuyer ? `JOIN network_campaigns_user_relations ncur ON ra.network_campaign_id = ncur.network_campaign_id` : ''
+  }
   WHERE
     ${mediaBuyer !== "admin" && mediaBuyer ? `ncur.user_id = ${mediaBuyer}` : "TRUE"}
+    ${network ? `AND ra.network = '${network}'` : ''}
   `
 
   const { rows } = await database.raw(query);
