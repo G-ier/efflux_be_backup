a3dafb7a7f16279df512184864d307958f02f013
Deni <deni.haxhiu13@gmail.com>
2024-08-26 13:07:52 +0200

    small query fix 2

--- src/modules/aggregates/repositories/AggregatesRepository.js
+++ src/modules/aggregates/repositories/AggregatesRepository.js
@@ -252,34 +252,46 @@ class AggregatesRepository {
 
   }
 
-  adAccountsGroupingQueryMaker(trafficSource, mediaBuyer, startDate, endDate){
+  adAccountsGroupingQueryMaker(trafficSource, mediaBuyer, startDate, endDate) {
+    console.log('In adAccountsGroupingQueryMaker', trafficSource, mediaBuyer, startDate, endDate);
 
     const query = `
+      WITH spend_aggregated AS (
         SELECT
-            s.ad_account_id,
-            s.ad_account_name,
-            s.traffic_source,
-            SUM(s.spend) AS total_spend,
-            SUM(s.spend_plus_fee) AS total_spend_plus_fee,
-            SUM(s.impressions) AS total_impressions,
-            SUM(s.clicks) AS total_clicks,
-            SUM(s.link_clicks) AS total_link_clicks,
-            SUM(s.ts_conversions) AS total_ts_conversions,
-            adc.id AS adc_id,
-            adc.provider_id AS provider_id
+          s.ad_account_id,
+          s.ad_account_name,
+          s.traffic_source,
+          CAST(SUM(s.spend) AS FLOAT) as spend,
+          CAST(SUM(s.spend_plus_fee) AS FLOAT) as spend_plus_fee,
+          CAST(SUM(s.impressions) AS INTEGER) as impressions,
+          CAST(SUM(s.link_clicks) AS INTEGER) as link_clicks,
+          CAST(SUM(s.ts_conversions) AS INTEGER) as ts_conversions
         FROM
-            spend s
-        JOIN
-            ad_accounts adc ON s.ad_account_id::text = adc.provider_id
-        JOIN
-            u_aa_map uam ON adc.id = uam.aa_id
+          spend s
         WHERE
-            s.traffic_source='${trafficSource}'
-            AND DATE(s.occurred_at) > '${startDate}'
-            AND DATE(s.occurred_at) <= '${endDate}'
-            ${mediaBuyer !== "admin" && mediaBuyer ? `AND uam.u_id = ${mediaBuyer}` : ""}
+          DATE(s.occurred_at AT TIME ZONE 'UTC' AT TIME ZONE 'America/Los_Angeles') > '${startDate}'
+          AND DATE(s.occurred_at AT TIME ZONE 'UTC' AT TIME ZONE 'America/Los_Angeles') <= '${endDate}'
         GROUP BY
-            s.ad_account_id, s.ad_account_name, s.traffic_source, adc_id, provider_id;
+          s.ad_account_id, s.ad_account_name, s.traffic_source
+      )
+      SELECT DISTINCT
+        sa.ad_account_id,
+        sa.ad_account_name as name,
+        sa.traffic_source,
+        sa.spend,
+        sa.spend_plus_fee,
+        sa.impressions,
+        sa.link_clicks,
+        sa.ts_conversions
+      FROM
+        spend_aggregated sa
+      JOIN
+        ad_accounts adc ON sa.ad_account_id::text = adc.provider_id
+      JOIN
+        u_aa_map uam ON adc.id = uam.aa_id
+      WHERE
+        ${mediaBuyer !== "admin" && mediaBuyer ? `uam.u_id = ${mediaBuyer}` : "TRUE"}
+        ${trafficSource ? `AND sa.traffic_source = '${trafficSource}'` : ''}
     `;
 
     return query;
